6e1dd49887bffa3170609d457c29f249a893d77c
Anna <anna@MacBook-Pro-Anna.local>
2020-12-10 23:15:05 +0300

    Initial commit

---
+++ src/test/java/org/example/LoginPage.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class LoginPage {
+
+    public WebDriver driver;
+    public LoginPage(WebDriver driver) {
+        PageFactory.initElements(driver, this);
+        this.driver = driver; }
+
+    @FindBy(xpath = "//*[@id=\"passp-field-login\"]")
+    private WebElement loginField;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/div[1]/form/div[3]/button")
+    private WebElement loginBtn;
+
+    @FindBy(xpath = "//*[@id=\"passp-field-passwd\"]")
+    private WebElement passwdField;
+
+    @FindBy(xpath = "//*[@id=\"root\"]/div/div[2]/div[2]/div/div/div[2]/div[3]/div/div/div/form/div[3]/button")
+    private WebElement loginBtn2;
+
+    public void inputLogin(String login) {
+        loginField.sendKeys(login);
+    }
+
+    public void inputPasswd(String passwd) {
+        passwdField.sendKeys(passwd);
+    }
+
+    public void clickLoginBtn() {
+        loginBtn.click();
+    }
+
+    public void clickLoginBtn2() {
+        loginBtn2.click();
+    }
+
+}
